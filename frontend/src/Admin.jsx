10e2601bf9895e7653a63d17b56263cf77847930
Suriya3621 <158826855+Suriya3621@users.noreply.github.com>
2024-02-17 00:07:38 +0530

    Update Admin.jsx

---
+++ frontend/src/Admin.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './Admin.css'
+function Admin(){
+  return(
+      <center class="container"> 
+  <p class="person text-light bg-dark "><i class="bi bi-person"></i></p>
+  <div class="detail">
+  <p>Admin name:Suriyaprakash</p>
+  <hr/>
+  <p>Email:suriyaprakashraja849@gmail.com</p>
+  </div><br/>
+    <Link to='/'><button class="btn btn-danger">Click here to login page</button></Link>
+  </center>
+    )
+}
+export default Admin;
