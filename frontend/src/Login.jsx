b22b52a6820644a9162028e63d078a8ceefa6613
Suriya3621 <158826855+Suriya3621@users.noreply.github.com>
2024-02-16 23:49:57 +0530

    Update Login.jsx

---
+++ frontend/src/Login.jsx
@@ -0,0 +1,34 @@
+import React,{useState} from "react";
+import {Link} from 'react-router-dom'
+import './Login.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+const Login = () => {
+ const [name,setName]= useState('')
+ const [room,setRoom]= useState('')
+    return (
+<div class="background">
+  <form>
+  <br/>
+    <div class="box container">
+    <br/><p class="h2 text-light">Room Entree</p><br/>
+      <input type="text" class="input " placeholder="Name"
+      value={name}
+      onChange={(e)=>setName(e.target.value)}
+      /><br/><br/>
+      <input type="text" name="" class="input"
+      placeholder="Room code"
+      value={room}
+      onChange={(e)=>setRoom(e.target.value)}
+      /><br/><br/>
+          <Link onClick={(name === '' || room === '') ? (e) => e.preventDefault() : null}
+      to={`/chat?name=${name}&room=${room}`}>
+      <button type="submit" class="btn btn-light">Enter Room</button>
+</Link>
+<br/>
+</div>
+        </form>
+      </div>
+);
+};
+
+export default Login;
