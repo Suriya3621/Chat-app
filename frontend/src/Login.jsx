3969b73314cb34f32cfe9f3fb2329e935b5108af
Suriya3621 <158826855+Suriya3621@users.noreply.github.com>
2024-02-17 00:26:24 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,7 +1,6 @@
 import React,{useState} from "react";
 import {Link} from 'react-router-dom'
 import './Login.css';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const Login = () => {
  const [name,setName]= useState('')
  const [room,setRoom]= useState('')
