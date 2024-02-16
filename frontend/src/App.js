81452929e0efc3e192fc5ccc1ccd3450df31e345
Suriya3621 <158826855+Suriya3621@users.noreply.github.com>
2024-02-16 18:15:27 +0000

    .gitignore file added

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Chat from './Chat';
+import Error from './Error';
+import Admin from './Admin';
+function App() {
+    return (
+            <div>
+                        <Routes>
+                                    
+                                                    <Route path="/" element={<Login />} />
+                                                  <Route path="*" element={<Error />} />
+                                                     <Route path="detail" element={<Admin />}/>
+                                                    <Route path="/chat" element={<Chat />} />
+                                                                 </Routes>
+                                                                                                                                        </div>
+                                                                                                                                            );
+                                                                                                                                            }
+
+                                                                 export default App;
