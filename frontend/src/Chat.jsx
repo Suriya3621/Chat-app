bc60d1051455e029b64e59d44247a07457ff2356
Suriya3621 <158826855+Suriya3621@users.noreply.github.com>
2024-02-17 13:16:48 +0530

    Update Chat.jsx

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -26,7 +26,7 @@ const Chat = () => {
 
         socket.emit('join', { user, room }, (err) => {
             if (err) {
-                // alert(err)
+                alert(err)
             }
         })
 
