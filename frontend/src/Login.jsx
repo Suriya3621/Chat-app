7c8133f32a128af0bcb2d572cda4b8c0cfbf23d0
Suriya3621 <158826855+Suriya3621@users.noreply.github.com>
2024-03-10 07:40:31 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -5,28 +5,37 @@ const Login = () => {
  const [name,setName]= useState('')
  const [room,setRoom]= useState('')
     return (
-<div class="background">
-  <form>
-  <br/>
-    <div class="box container div text-light">
-    <br/><p class="h2 text-light">Join the Room</p><br/>
-      <input type="text" class="input " placeholder="Name"
-      value={name}
-      onChange={(e)=>setName(e.target.value)}
-      /><br/><br/>
-      <input type="text" name="" class="input"
-      placeholder="Room code"
-      value={room}
-      onChange={(e)=>setRoom(e.target.value)}
-      /><br/><br/>
+  <div class="container ">
+    <div class="circleyo"></div>
+    <div class="login-container">
+      <h2 class="mb-4 text-center">Join the room</h2><br/><br/>
+      <form>
+        <div class="mb-3 input">
+          <input type="text" id="username" class="username-input text-dark" required 
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+          />
+          <label for="username">Username</label>
+        </div><br/>
+        <div class="mb-3 input">
+          <input type="text" id="room-code" class="room-code-input" required 
+          value={room}
+          onChange={(e)=>setRoom(e.target.value)}
+          />
+          <label for="room-code">Room code</label>
+        </div>
+        <div class="d-grid">
           <Link onClick={(name === '' || room === '') ? (e) => e.preventDefault() : null}
       to={`/chat?name=${name}&room=${room}`}>
-      <button type="submit" class="btn btn-light">Join</button>
-</Link>
-<br/>
+          <button class="btn btn-primary btn-block">Join</button>
+          </Link>
+        </div>
+          
+      </form>
+              <div class="circlegy">
+    </div>
+  </div>
 </div>
-        </form>
-      </div>
 );
 };
 
